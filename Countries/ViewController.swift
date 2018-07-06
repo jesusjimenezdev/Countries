87dade7dde0b153dd2ec0b4285b4bb3ea72d73a1
Jesus <jesus@iMac-de-Jesus.local>
2018-07-06 20:29:43 +0200

    first commit

--- Countries/ViewController.swift
+++ Countries/ViewController.swift
@@ -1,25 +1,34 @@
-//
-//  ViewController.swift
-//  Countries
-//
-//  Created by Jesus on 6/7/18.
-//  Copyright © 2018 Jesus. All rights reserved.
-//
-
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        datosWeb()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func datosWeb(){
+        DispatchQueue.main.async {
+            Alamofire.request("http://restcountries.eu/rest/v2/all").responseJSON(completionHandler: { (resultado) in
+                switch resultado.result{
+                case .success(let value):
+                    
+                    let json = JSON(value)
+                    for item in json.array! {
+                        let nombre = item["name"].stringValue
+                        let capital = item["capital"].stringValue
+                        print(nombre, capital)
+                        //let usuarios = Users(nombre: nombre, email: email, calle: calle)
+                        //self.lista.append(usuarios)
+                    }
+                    //self.tabla.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            })
+        }
     }
-
-
 }
 
